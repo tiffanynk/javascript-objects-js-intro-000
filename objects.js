d21ec4b2091136a80cfff8ec997587a7ec2b9f77
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-27 19:19:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle
-  })
+  return Object.assign({}, playlist, [artistName]: songTitle
+  )
 }
