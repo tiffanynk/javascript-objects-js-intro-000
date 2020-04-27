1ee57aedb620c2b28323dd57adf0b0c6fe118410
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-27 19:45:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName;
+}
